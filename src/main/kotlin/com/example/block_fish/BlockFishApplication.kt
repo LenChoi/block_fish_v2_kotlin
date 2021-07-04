73d242f7debbee0fca69791a01219fb18cabe527
Len <len.choi@cyworldlabs.com>
2021-07-04 16:07:50 +0900

    first commit

---
+++ src/main/kotlin/com/example/block_fish/BlockFishApplication.kt
@@ -0,0 +1,11 @@
+package com.example.block_fish
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlockFishApplication
+
+fun main(args: Array<String>) {
+    runApplication<BlockFishApplication>(*args)
+}
